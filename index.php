8266f6289dcd06ab0a5946b0796a2e18a62cf68f
MartinR01 <martin.ryba01@gmail.com>
2018-04-23 11:01:12 +0200

    added bootstrap library and basic files for the structure we are going to use

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- LOGIN SCREEN
+		if user already logged in, redirect to home
+-->
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Project Management System</title>
+    <!-- CSS Bootstrap -->
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <!-- CSS Our-->
+      <link rel="stylesheet" href="css/design.css">
+	  <link rel="stylesheet" href="css/login.css">
+
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			
+		</div>
+	</div>
+    <!-- JS for Bootstrap -->
+  <script src="js/bootstrap.js"></script>
+</body>
+</html>
