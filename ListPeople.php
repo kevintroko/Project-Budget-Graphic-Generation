7e3fe082a1a5482cecb50ead3008f102ce74654b
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2018-04-24 13:58:58 +0200

    Merge resolved

---
+++ ListPeople.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Project Management System</title>
+		<!-- CSS other -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<!-- CSS Our-->
+		<link rel="stylesheet" href="css/design.css">
+		<link rel="stylesheet" href="css/list.css">
+    <!-- Icons links -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bottom-border">
+  <a class="navbar-brand" href="index.php">Project Budget Manager</a>
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="profile.php">Name Lastname</a>
+      <a class="nav-item nav-link" href="#Notification">Notification</a>
+      <a class="nav-item nav-link" href="index.php">Log out</a>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="title">
+          <span id=active>People</span> <a href="listProject.php" id=inactive>Project</a>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <table class="table">
+  <thead>
+    <tr class="table-row">
+      <th scope="col">Name <i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Lastname <i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Email <i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Role <i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Projects <i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Workflow <i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>John</td>
+      <td>Bergstrom</td>
+      <td>john.bergstrom@mdh.se</td>
+      <td>professor</td>
+      <td>3</td>
+      <td>60%</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Bergstrom</td>
+      <td>john.bergstrom@mdh.se</td>
+      <td>professor</td>
+      <td>3</td>
+      <td>60%</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Bergstrom</td>
+      <td>john.bergstrom@mdh.se</td>
+      <td>professor</td>
+      <td>3</td>
+      <td>60%</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Bergstrom</td>
+      <td>john.bergstrom@mdh.se</td>
+      <td>professor</td>
+      <td>3</td>
+      <td>60%</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Bergstrom</td>
+      <td>john.bergstrom@mdh.se</td>
+      <td>professor</td>
+      <td>3</td>
+      <td>60%</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Bergstrom</td>
+      <td>john.bergstrom@mdh.se</td>
+      <td>professor</td>
+      <td>3</td>
+      <td>60%</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  </div>
+
+
+
+    <!-- JS for Bootstrap -->
+  <script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ ListProject.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Project Management System</title>
+		<!-- CSS other -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<!-- CSS Our-->
+		<link rel="stylesheet" href="css/design.css">
+		<link rel="stylesheet" href="css/list.css">
+		<!-- Icons links -->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bottom-border">
+  <a class="navbar-brand" href="index.php">Project Budget Manager</a>
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="profile.php">Name Lastname</a>
+      <a class="nav-item nav-link" href="#Notification">Notification</a>
+      <a class="nav-item nav-link" href="index.php">Log out</a>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+				<div class="title">
+        <a href="ListPeople.php" id=inactive>People</a> <span id=active>Project</span>
+				</div>
+      </div>
+    </div>
+    <div class="row">
+      <table class="table">
+  <thead>
+    <tr class="table-row">
+      <th scope="col">Name<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Start date<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Deadline<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Budget<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Workers<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Partners<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Funding agency<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+      <th scope="col">Workload<i class="material-icons vertical-align-middle padding-bottom-3">arrow_drop_down</i></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Budget project manager</td>
+      <td>09/04/18</td>
+      <td>14/10/18</td>
+      <td>584,200/1,000,000</td>
+      <td>1</td>
+      <td>2</td>
+      <td>1</td>
+      <td>70%</td>
+    </tr>
+    <tr>
+      <td>Budget project manager</td>
+      <td>09/04/18</td>
+      <td>14/10/18</td>
+      <td>584,200/1,000,000</td>
+      <td>1</td>
+      <td>2</td>
+      <td>1</td>
+      <td>70%</td>
+    </tr>
+    <tr>
+      <td>Budget project manager</td>
+      <td>09/04/18</td>
+      <td>14/10/18</td>
+      <td>584,200/1,000,000</td>
+      <td>1</td>
+      <td>2</td>
+      <td>1</td>
+      <td>70%</td>
+    </tr>
+    <tr>
+      <td>Budget project manager</td>
+      <td>09/04/18</td>
+      <td>14/10/18</td>
+      <td>584,200/1,000,000</td>
+      <td>1</td>
+      <td>2</td>
+      <td>1</td>
+      <td>70%</td>
+    </tr>
+    <tr>
+      <td>Budget project manager</td>
+      <td>09/04/18</td>
+      <td>14/10/18</td>
+      <td>584,200/1,000,000</td>
+      <td>1</td>
+      <td>2</td>
+      <td>1</td>
+      <td>70%</td>
+    </tr>
+    <tr>
+      <td>Budget project manager</td>
+      <td>09/04/18</td>
+      <td>14/10/18</td>
+      <td>584,200/1,000,000</td>
+      <td>1</td>
+      <td>2</td>
+      <td>1</td>
+      <td>70%</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  </div>
+
+    <!-- JS for Bootstrap -->
+  <script src="js/bootstrap.js"></script>
+</body>
+</html>
