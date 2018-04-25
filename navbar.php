ca44beaa42cf05a8ba1d032bd8f6aef4951d3ef8
MartinR01 <martin.ryba01@gmail.com>
2018-04-25 11:03:50 +0200

    updated the navbar

--- navbar.php
+++ navbar.php
@@ -1,16 +1,19 @@
 <link rel="stylesheet" href="css/notifications.css">
 
-<nav class="navbar navbar-expand-lg navbar-light bottom-border">
-	<a class="navbar-brand" href="index.php">Project Budget Manager</a>
-	<div class="navbar-nav">
-		<a class="nav-item nav-link" href="profile.php">Name Lastname</a>
-		<a class="nav-item nav-link" href="#Notification" 
+<nav class="navbar navbar-expand-lg navbar-light bottom-border ">
+	<a class="navbar-brand" href="home.php">Project Budget Manager</a>
+	<div class="navbar-nav w-100">
+	
+		<a class="nav-item nav-link" href="list_people.php">Database</a>
+		
+		
+		<a class="nav-item nav-link ml-auto" href="#"
 			data-toggle="popover" 
 			data-placement="bottom" 
 			data-html="true" >
-				Notification
+				Notifications
 		</a>
-		
+		<a class="nav-item nav-link" href="profile.php">Name Lastname</a>
 		<a class="nav-item nav-link" href="index.php">Log out</a>
 	</div>
 	
