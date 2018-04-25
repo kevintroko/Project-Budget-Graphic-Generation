e74d2a2983db194354cf8ca61bc08ad1358d5a39
MartinR01 <martin.ryba01@gmail.com>
2018-04-25 10:21:19 +0200

    Added basic notification bar

--- navbar.php
+++ navbar.php
@@ -1,9 +1,18 @@
+<link rel="stylesheet" href="css/notifications.css">
+
 <nav class="navbar navbar-expand-lg navbar-light bottom-border">
 	<a class="navbar-brand" href="index.php">Project Budget Manager</a>
 	<div class="navbar-nav">
 		<a class="nav-item nav-link" href="profile.php">Name Lastname</a>
-		<a class="nav-item nav-link" href="#Notification">Notification</a>
+		<a class="nav-item nav-link" href="#Notification" 
+			data-toggle="popover" 
+			data-placement="bottom" 
+			data-html="true" >
+				Notification
+		</a>
+		
 		<a class="nav-item nav-link" href="index.php">Log out</a>
-
 	</div>
+	
+	<?php include_once ("notifications.php");?>
 </nav>
---
+++ notifications.php
@@ -0,0 +1,31 @@
+<div id="notifications-container" style="display:none" >
+
+	<!-- If no notifications, display this -->
+	
+
+	<!-- Add notifications here -->
+	<div class="notification rounded m-2 p-1">
+		<h5>
+			Someone stole your bike!
+		</h5>
+		<p class="text-right font-weight-light font-italic m-0">
+			5 mins ago
+		</p>
+	</div>
+	
+	<hr class="m-2">
+	
+	<div class="notification rounded m-2 p-1">
+		<h5>
+			Your new bike has arrived!
+		</h5>
+		<p class="text-right font-weight-light font-italic  m-0">
+			20 mins ago
+		</p>
+	</div>
+	
+	<!-- Mark as read button -->
+	<div class="m-2">
+		<button class="btn  btn-sm btn-block">Mark as read</button>
+	</div>
+</div>
