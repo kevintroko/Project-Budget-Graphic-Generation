1f6eee62158055f4f01138922c6d071677721467
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2018-05-03 16:09:44 +0200

    Home page divided into components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './css/App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Login from './components/Login';
+import Filter from './components/Filter';
+import AddIcon from './components/AddIcon';
 
 class App extends Component {
   render() {
       return (
         <div className="App">
           <Navbar/>
+          <Filter/>
+          <AddIcon/>
           <Home/>
         </div>
       );
