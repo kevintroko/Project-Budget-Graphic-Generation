890996437eed038cec878cec24786a2e464f6c7e
MartinR01 <martin.ryba01@gmail.com>
2018-05-07 14:49:02 +0200

    Added login page     the login button gets dynamically disabled when username or password is not filled out     data is stored in component's state     almost entire page written w/ reactstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,73 @@
 import React, {Component} from 'react';
+import {Form,FormGroup,Input,InputGroup,InputGroupAddon,Col,Row,Container} from 'reactstrap';
 import '../css/Login.css';
 
 class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username:"",
+      password:""
+    };
+  }
+  isInvalid(){
+    return !((this.state.username.length > 0)&&(this.state.password.length > 0));
+  }
+
+  handleChange = (name, e) =>{
+    this.setState({[name]: e.target.value});
+  }
+
   render() {
-    if (this.props.signedIn == true) {
-      return <h1>GO AWAY</h1>;
-    } else {
-      return <h1>Hi there Mark</h1>;
-    }
+    return(
+      <Container>
+  			<Row>
+
+  				{/* Login Box */}
+          <Col xs={{size: 12}} sm={{size: 8, offset: 2}}  id="main-login">
+  					<h1 class="text-center">Project Budget Manager</h1>
+  					<br/>
+
+  					{/* Login Form */}
+  					<Row>
+  						<Col sm={{size: 8, offset: 2}}>
+
+
+                <Form
+                  method="post">
+                  <FormGroup>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <span class="input-group-text" id="basic-addon1"><i class="material-icons">email</i></span>
+                      </InputGroupAddon>
+                      <Input type="text" name="email" placeholder="Enter your Email address" aria-label="Username" aria-describedby="basic-addon1"
+                          value={this.state.username}
+                          onChange={(e) => this.handleChange("username", e)}/>
+                    </InputGroup>
+                  </FormGroup>
+
+                  <FormGroup>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <span class="input-group-text" id="basic-addon1"><i class="material-icons">lock</i></span>
+                      </InputGroupAddon>
+                      <Input type="password" name="password" class="form-control" placeholder="Enter your Password" aria-label="Password" aria-describedby="basic-addon1"
+                      value={this.state.pass}
+                      onChange={(e) => this.handleChange("password", e)}/>
+                    </InputGroup>
+                  </FormGroup>
+
+  								<br/>
+  								<input class="btn btn-lg btn-block" type="submit" value="Login" disabled={this.isInvalid()}/>
+                </Form>
+
+
+  						</Col>
+  					</Row>
+  				</Col>
+  			</Row>
+  		</Container>
+    );
   }
 };
-/*https://medium.com/the-codelog/intro-to-react-js-a37696fd99af*/
 export default Login;
