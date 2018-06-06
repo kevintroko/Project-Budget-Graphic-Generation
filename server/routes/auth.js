593f898ee4383d662d1cb61e58195e07caf08976
ikatsadas <johnkats5896@gmail.com>
2018-06-06 22:24:55 +0200

    edit form queries

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -266,7 +266,167 @@ router.post('/newproject', (req, res, next) => {
 
 
 
+router.post('/updateproject', (req, res, next) => {
 
+  const validationResult = validateNewProjectPage(req.body);
+  if (!validationResult.success) {
+    return res.status(400).json({
+      success: false,
+      message: validationResult.message,
+      errors: validationResult.errors
+    });
+  }
+
+  connection.query("select project_view.code from project_view where code = '"+req.body.project_code+"'",function(err,rows){
+    if (err)
+      return done(err);
+    if (rows.length!=0) {
+      var newProjectMysql = new Object();
+      newProjectMysql.name = req.body.name;
+      newProjectMysql.owner = req.body.email;
+      newProjectMysql.code = req.body.project_code;
+      newProjectMysql.budget = req.body.project_budget;
+      newProjectMysql.description = req.body.project_desc;
+      newProjectMysql.start_date = yyyymmdd(req.body.project_start);
+      newProjectMysql.deadline = yyyymmdd(req.body.project_deadline);
+      var newParticipantsMysql = new Object();
+      newParticipantsMysql.participants=JSON.parse(req.body.values);
+      connection.query("UPDATE project_view SET deadline='"+newProjectMysql.deadline+"' AND owner = '"+newProjectMysql.owner+"' AND start_date='"+newProjectMysql.start_date+"' AND description='"+newProjectMysql.description+"' AND budget='"+newProjectMysql.budget+"' AND name='"+newProjectMysql.name+"' WHERE code='"+newProjectMysql.code+"'",function(err,rows){});
+       newParticipantsMysql.participants.map(p=>{
+        var from_year=p.range.from.year;
+        var from_month=p.range.from.month;
+        var to_year=p.range.to.year;
+        var to_month=p.range.to.month;
+        if (from_month<10){
+          var from=new Date(from_year+"-0"+from_month+"-01");
+        }else{
+          var from=new Date(from_year+"-"+from_month+"-01");
+        }
+        if (to_month<10){
+          var to=new Date(to_year+"-0"+to_month+"-01");
+        }else{
+          var to=new Date(to_year+"-"+to_month+"-01");
+        }
+      connection.query("select * from has_view where person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"' AND hiring_date='"+yyyymmdd(from)+"'",function(err,rows){
+          if (err)
+            return done(err);
+          if (rows.length==0) {
+            var insertQuery = "INSERT INTO has_view (project_code, person_code, end_date, hiring_date, job_description, workload) values ('" + newProjectMysql.code +"','"+ p.email +"','"+yyyymmdd(to) +"','"+ yyyymmdd(from) +"','"+ p.description+"','"+ p.workload+"')";
+            console.log(insertQuery);
+            connection.query(insertQuery,function(err,rows){
+                newParticipantsMysql.id = rows.insertId;
+            });
+          }else{
+             connection.query("UPDATE has_view SET job_description='"+p.job_description+"' AND workload='"+p.workload+"' AND end_date='"+yyyymmdd(to)+"' WHERE hiring_date='"+yyyymmdd(from)+"' AND  person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"'",function(err,rows){});
+          }
+        });
+      }) 
+    } else {
+                      var newProjectMysql = new Object();
+                      newProjectMysql.name = req.body.name;
+                      newProjectMysql.owner = req.body.email;
+                      newProjectMysql.code = req.body.project_code;
+                      newProjectMysql.budget = req.body.project_budget;
+                      newProjectMysql.description = req.body.project_desc;
+                      newProjectMysql.start_date = yyyymmdd(req.body.project_start);
+                      newProjectMysql.deadline = yyyymmdd(req.body.project_deadline);
+                      var newParticipantsMysql = new Object();
+                      newParticipantsMysql.participants=JSON.parse(req.body.values);
+                      var insertQuery = "INSERT INTO project_view (name, code, owner, budget, description, start_date, deadline) values ('" + newProjectMysql.name +"','"+ newProjectMysql.code +"','"+newProjectMysql.owner +"','"+newProjectMysql.budget +"','"+ newProjectMysql.description+"','"+ newProjectMysql.start_date+"','"+newProjectMysql.deadline+"')";
+                      console.log(insertQuery);
+                      connection.query(insertQuery,function(err,rows){
+                        newProjectMysql.id = rows.insertId;
+                      });
+
+                      newParticipantsMysql.participants.map(p=>{
+                        console.log("~~~~~~~~~~~~");
+                        console.log("Email");
+                        console.log(p.email);
+                        console.log("Range");
+                        console.log(p.range);
+                        console.log("Workload");
+                        console.log(p.workload);
+                        console.log("Description");
+                        console.log(p.description);
+                        console.log("~~~~~~~~~~~~");
+                        var from_year=p.range.from.year;
+                        var from_month=p.range.from.month;
+                        var to_year=p.range.to.year;
+                        var to_month=p.range.to.month;
+                        if (from_month<10){
+                          var from=new Date(from_year+"-0"+from_month+"-01");
+                        }else{
+                          var from=new Date(from_year+"-"+from_month+"-01");
+                        }
+                        if (to_month<10){
+                          var to=new Date(to_year+"-0"+to_month+"-01");
+                        }else{
+                          var to=new Date(to_year+"-"+to_month+"-01");
+                        }
+
+                        connection.query("select * from has_view where person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"'",function(err,rows){
+                          if (err)
+                            return done(err);
+                          if (rows.length!=0) {
+                            rows.map(r=>{
+                              var old_from=new Date(r.hiring_date);
+                              var old_to=new Date(r.end_date);
+                              //overlaping cases with other has rows
+                              if (old_from.getTime()<from.getTime() && old_to.getTime()>from.getTime() && old_to.getTime()<to.getTime() ){
+                                //change the old one according to the new one
+                                //UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;
+                                connection.query("UPDATE has_view SET end_date='"+yyyymmdd(from)+"' where person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"' AND hiring_date='"+yyyymmdd(r.hiring_date)+"' AND end_date='"+yyyymmdd(r.end_date)+"'",function(err,rows){});
+                                console.log("FIRST");
+                                console.log("IF old.from < new.from < old.to < new.to THEN OLD=[old.from,new.from] and NEW=[new.from,new.to]");
+                              }else if (old_from.getTime()<to.getTime() && old_to.getTime()>to.getTime() &&from.getTime()<old_from.getTime()){
+                                //change the old one according to the new one
+                                //UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;
+                                connection.query("UPDATE has_view SET hiring_date='"+yyyymmdd(to)+"' where person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"' AND hiring_date='"+yyyymmdd(r.hiring_date)+"' AND end_date='"+yyyymmdd(r.end_date)+"'",function(err,rows){});
+                                console.log("SECOND");
+                                console.log("IF new.from < old.from < new.to < old.to THEN OLD=[new.to,old.to] and NEW=[new.from,new.to]");
+                              }else if(old_from.getTime()<from.getTime() && old_to.getTime()>to.getTime()){
+                                //split the old one according to the new one and insert the two parts of the old one
+                                connection.query("UPDATE has_view SET end_date='"+yyyymmdd(from)+"' where person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"' AND hiring_date='"+yyyymmdd(r.hiring_date)+"' AND end_date='"+yyyymmdd(r.end_date)+"'",function(err,rows){});
+                                connection.query("INSERT INTO has_view (project_code, person_code, end_date, hiring_date, job_description, workload) values ('" + newProjectMysql.code +"','"+ p.email +"','"+yyyymmdd(old_to) +"','"+ yyyymmdd(to) +"','"+ r.job_description+"','"+ r.workload+"')",function(err,rows){});
+                                console.log("THIRD");
+                                console.log("IF old.from < new.from < new.to < old.to THEN OLD1=[old.from,new.from] and NEW=[new.from,new.to] and OLD2=[new.to,old.to]"); 
+                              }else if(old_from.getTime()>from.getTime() && old_to.getTime()<to.getTime()){
+                                //replace the old one with the new one
+                               connection.query("DELETE FROM has_view WHERE person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"' AND hiring_date='"+yyyymmdd(r.hiring_date)+"' AND end_date='"+yyyymmdd(r.end_date)+"'",function(err,rows){});
+                                 console.log("FOURTH");
+                                console.log("IF new.from < old.from < old.to < new.to THEN NEW=[new.from,new.to] (DELETE OLD)"); 
+                              }
+                            })
+                          }
+                        });
+
+                        connection.query("select * from has_view where person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"' AND hiring_date='"+p.hiring_date+"'",function(err,rows){
+                          if (err)
+                            return done(err);
+                          if (rows.length==0) {
+                            var insertQuery = "INSERT INTO has_view (project_code, person_code, end_date, hiring_date, job_description, workload) values ('" + newProjectMysql.code +"','"+ p.email +"','"+yyyymmdd(to) +"','"+ yyyymmdd(from) +"','"+ p.description+"','"+ p.workload+"')";
+                            console.log(insertQuery);
+                            connection.query(insertQuery,function(err,rows){
+                                newParticipantsMysql.id = rows.insertId;
+                            });
+                          }else{
+                            connection.query("DELETE FROM has_view WHERE person_code = '"+p.email+"' AND project_code='"+newProjectMysql.code+"' AND hiring_date='"+yyyymmdd(r.hiring_date)+"' AND end_date='"+yyyymmdd(r.end_date)+"'",function(err,rows){});
+                            var insertQuery = "INSERT INTO has_view (project_code, person_code, end_date, hiring_date, job_description, workload) values ('" + newProjectMysql.code +"','"+ p.email +"','"+yyyymmdd(to) +"','"+ yyyymmdd(from) +"','"+ p.description+"','"+ p.workload+"')";
+                            console.log(insertQuery);
+                            connection.query(insertQuery,function(err,rows){
+                                newParticipantsMysql.id = rows.insertId;
+                            });
+                          }
+                        });
+                      }) 
+                       
+     }
+     return res.status(200).json({
+                              success: true,
+                              message: 'You have successfully created a new project with participants!'
+                            });
+   });
+  });
 
 
 router.post('/signup', (req, res, next) => {
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,103 +3,103 @@ import Chart from './Chart';
 import {Table} from './Table';
 import {Row,Col,Container} from 'reactstrap';
 
-class Project extends Component {
-  constructor(props) {
-    super(props);
-    this.state={project:{percentage:0,start_date:new Date(),deadline:new Date(),budget:0}, persons:[]};
-  }
+// class Project extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state={project:{percentage:0,start_date:new Date(),deadline:new Date(),budget:0}, persons:[]};
+//   }
 
-  componentWillMount(){
-   this.callApi().then(data => (this.getData(data)));
-  }
+//   componentWillMount(){
+//    this.callApi().then(data => (this.getData(data)));
+//   }
 
-  getData(data){
-    let pr=Object.assign({}, this.state.project);
-    let pers={salary:0,socialfactor:0,workload:0,firstDay:new Date(),LastDay:new Date()};
+//   getData(data){
+//     let pr=Object.assign({}, this.state.project);
+//     let pers={salary:0,socialfactor:0,workload:0,firstDay:new Date(),LastDay:new Date()};
 
-    data.map(p=> (
-      pr.budget=p.budget,
-      pr.start_date=new Date(p.start_date),
-      pr.deadline=new Date(p.deadline),
-      pr.percentage=p.expectedPercentage,
-      pers.salary=p.salary,
-      pers.socialfactor=p.socialfactor,
-      pers.workload=p.workload,
-      pers.firstDay=new Date(p.hiring_date),
-      pers.LastDay=new Date(p.end_date)
-    ))
-    this.setState(prevState => ({ persons: [...prevState.persons, pers]}))
-    console.log(this.state.values);
-    console.log(data);
-    console.log(pr);
-  }
+//     data.map(p=> (
+//       pr.budget=p.budget,
+//       pr.start_date=new Date(p.start_date),
+//       pr.deadline=new Date(p.deadline),
+//       pr.percentage=p.expectedPercentage,
+//       pers.salary=p.salary,
+//       pers.socialfactor=p.socialfactor,
+//       pers.workload=p.workload,
+//       pers.firstDay=new Date(p.hiring_date),
+//       pers.LastDay=new Date(p.end_date)
+//     ))
+//     this.setState(prevState => ({ persons: [...prevState.persons, pers]}))
+//     console.log(this.state.values);
+//     console.log(data);
+//     console.log(pr);
+//   }
 
-  async callApi(){
-    const response = await fetch('http://localhost:5000/project_info?code='+'ASS34');
-    const body = await response.json();
-  if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
+//   async callApi(){
+//     const response = await fetch('http://localhost:5000/project_info?code='+'ASS34');
+//     const body = await response.json();
+//   if (response.status !== 200) throw Error(body.message);
+//     return body;
+//   };
 
-  render() {
-    let dates, datesP;
-		let time = Math.round((((((this.state.project.deadline-this.state.project.startDate)/1000)/60)/60)/24)/30);
-		let cost=0;
-		let budgetData = [];
-		let averageLine = [];
-		let expected_budget = this.state.project.budget*((100-this.state.project.percentage)*0.01);
-    //Push the project budget into the graph so push 2000
-    budgetData.push(this.state.project.budget);
-    averageLine.push(expected_budget);
-    this.state.persons.map(pers=>(pers.cost=(pers.salary)*(pers.socialfactor)*(pers.workload/100)));
-    let peopleToADD;
-    let currentmonth,currentmonthPerson;
-    //GET array of months )
-    getBudgetPerMonth(this.state.project.start_date.getFullYear()+" "+(this.state.project.start_date.getMonth()),(this.state.project.deadline.getFullYear())+" "+(this.state.project.deadline.getMonth()));
-    this.state.persons.map(pers=>(
-    getPersonPerMonth((pers.firstDay.getFullYear())+" "+(pers.firstDay.getMonth()),(pers.LastDay.getFullYear())+" "+(pers.LastDay.getMonth()),pers)
-    ))
+//   render() {
+//     let dates, datesP;
+// 		let time = Math.round((((((this.state.project.deadline-this.state.project.startDate)/1000)/60)/60)/24)/30);
+// 		let cost=0;
+// 		let budgetData = [];
+// 		let averageLine = [];
+// 		let expected_budget = this.state.project.budget*((100-this.state.project.percentage)*0.01);
+//     //Push the project budget into the graph so push 2000
+//     budgetData.push(this.state.project.budget);
+//     averageLine.push(expected_budget);
+//     this.state.persons.map(pers=>(pers.cost=(pers.salary)*(pers.socialfactor)*(pers.workload/100)));
+//     let peopleToADD;
+//     let currentmonth,currentmonthPerson;
+//     //GET array of months )
+//     getBudgetPerMonth(this.state.project.start_date.getFullYear()+" "+(this.state.project.start_date.getMonth()),(this.state.project.deadline.getFullYear())+" "+(this.state.project.deadline.getMonth()));
+//     this.state.persons.map(pers=>(
+//     getPersonPerMonth((pers.firstDay.getFullYear())+" "+(pers.firstDay.getMonth()),(pers.LastDay.getFullYear())+" "+(pers.LastDay.getMonth()),pers)
+//     ))
 
-    for (let x = 0; x < 12; x++) {
-      //BEGIN
-      peopleToADD = [];
-      currentmonth = dates.shift();
-                  this.state.persons.map(pers=>{
-                    if(pers.monthsWorking[0]===currentmonth){
-                      currentmonthPerson = pers.monthsWorking.shift();
-                      if(currentmonth===currentmonthPerson){
-                        peopleToADD.push(pers);
-                      }
-                    }
-                  }
-      )
+//     for (let x = 0; x < 12; x++) {
+//       //BEGIN
+//       peopleToADD = [];
+//       currentmonth = dates.shift();
+//                   this.state.persons.map(pers=>{
+//                     if(pers.monthsWorking[0]===currentmonth){
+//                       currentmonthPerson = pers.monthsWorking.shift();
+//                       if(currentmonth===currentmonthPerson){
+//                         peopleToADD.push(pers);
+//                       }
+//                     }
+//                   }
+//       )
 
 
-      for (var i = 0; i < peopleToADD.length; i++) {
-        cost+=peopleToADD[i].cost;
-      }
-      //Push the 80% of line
-      averageLine.push(expected_budget);
-      this.state.project.budget=this.state.project.budget-cost;
-      budgetData.push(this.state.project.budget);
-      cost=0;
-      //END
-    }
+//       for (var i = 0; i < peopleToADD.length; i++) {
+//         cost+=peopleToADD[i].cost;
+//       }
+//       //Push the 80% of line
+//       averageLine.push(expected_budget);
+//       this.state.project.budget=this.state.project.budget-cost;
+//       budgetData.push(this.state.project.budget);
+//       cost=0;
+//       //END
+//     }
 
-    return (
-      <div>
-        <div className="title">Project</div>
-        <div className="row panel flex-center-vertically">
-          <div className="col-12">
-            <Chart data={budgetData} averageLine={averageLine} type="budget"
-              startDate={this.state.project.start_date}
-              endDate={this.state.project.deadline}/>
-          </div>
-        </div>
-      </div>
-    );
-  }
-};
+//     return (
+//       <div>
+//         <div className="title">Project</div>
+//         <div className="row panel flex-center-vertically">
+//           <div className="col-12">
+//             <Chart data={budgetData} averageLine={averageLine} type="budget"
+//               startDate={this.state.project.start_date}
+//               endDate={this.state.project.deadline}/>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// };
 
 
 export const Project = (props) => {
